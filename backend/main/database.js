3c2b9c714ba7593ea73c3b3710394b0730c7327a
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-03 01:12:53 +0700

    backend database from PORT to DB_PORT

--- backend/main/database.js
+++ backend/main/database.js
@@ -1,11 +1,11 @@
 import MySQL from "mysql2";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const database = MySQL.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
